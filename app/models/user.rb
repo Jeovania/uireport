8bb9d9c263d84ff7d9032e13533772639640df66
JeoLopes <jeovaniaabreu@gmail.com>
2014-08-06 14:12:37 -0300

    devise email out

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   def convidado(u_id, p_id)
     Profile::Invite.exists?(:user_id => u_id, :project_id => p_id)
